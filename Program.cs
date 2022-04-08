f4d6475ad88503f79bf7891970696e68a9a4adc7
Anastasia65 <anst03@mail.ru>
2022-04-09 01:49:15 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AtbashCipher
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter messages: ");
+            string userInput = Console.ReadLine();
+            Console.WriteLine();
+
+
+            string Alphabet = "abcdefghijklmnopqrstuvwxyz";
+            string AlphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string result = "";
+
+
+            foreach (char c in userInput)
+            {
+                for (int i = 0; i < Alphabet.Length; i++)
+                {
+                    if (c == Alphabet[i])
+                    {
+                        result += Alphabet[Alphabet.Length - 1 - i];
+
+                    }
+                    if (c == AlphabetUpper[i])
+                    {
+                        result += AlphabetUpper[AlphabetUpper.Length - 1 - i];
+
+                    }
+                }
+
+            }
+
+            Console.WriteLine("Encoded messages: " + result);
+            Console.WriteLine();
+
+            Console.Write("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
